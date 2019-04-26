e78bd4f4b170b0217c390ec4a48e73d655846f14
Haofan Zheng <hazheng@mix.wvu.edu>
2019-04-25 22:09:04 -0700

    Added maven plugin code for C binding libs; Fixed compile err.

--- decentdht/src/main/java/com/yahoo/ycsb/db/DecentDHTClient.java
+++ decentdht/src/main/java/com/yahoo/ycsb/db/DecentDHTClient.java
@@ -5,17 +5,16 @@ import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.Status;
 import com.yahoo.ycsb.StringByteIterator;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
 import org.json.JSONObject;
 
-import com.Decent.DhtClientBinding;
+import com.decent.dht.DhtClientBinding;
+import com.decent.dht.DhtClientBindingException;
 
 /**
  * YCSB binding for DecentDHT.
@@ -30,33 +29,41 @@ public class DecentDHTClient extends DB {
   private boolean isInited = false;
 
   public void init() throws DBException {
-    // Initialize the DHT.
-    DhtClientBinding.init();
+    
+    try {
+      DhtClientBinding.init();
+    } catch (DhtClientBindingException e) {
+      throw new DBException(e.getMessage());
+    }
     isInited = true;
   }
 
   public void cleanup() throws DBException {
+    
     if (isInited) {
-    // Deconstruct objects initilized in the init() function.
-      DhtClientBinding.cleanup();
+      try {
+        DhtClientBinding.cleanup();
+      } catch (DhtClientBindingException e) {
+        throw new DBException(e.getMessage());
+      }
       isInited = false;
     }
   }
 
   @Override
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    
     // byte[] buffer;
     String buffer;
-    
+
     // table is ignored in this function.
     // Given the key, retrieve the value from DHT
     // Convert the data retrieved to Json doc.
 
     try {
-      // Retrieve the buffer by the data retrived. Example: readResult.getBuffer().get(buffer);
       buffer = DhtClientBinding.read(key);
-    } catch (RadosException e) {
-      return new Status("ERROR-" + e.getReturnValue(), e.getMessage());
+    } catch (DhtClientBindingException e) {
+      return new Status("ERROR ", e.getMessage());
     }
 
     // The rest of the code should be similar in our implementation. 
@@ -73,8 +80,9 @@ public class DecentDHTClient extends DB {
 
 
   @Override
-  public Status insert(String table, String key, Map<String, ByteIterator> values) {
-
+  public Status insert(String table, String key, Map<String, ByteIterator> values)
+  {
+    
     // table is ignored again.
     // No need to change the codes below.
     JSONObject json = new JSONObject();
@@ -85,27 +93,29 @@ public class DecentDHTClient extends DB {
     try {
       // Write the key and the value to our DHT here. Example: ioctx.write(key, json.toString());
       DhtClientBinding.insert(key, json.toString());
-    } catch (RadosException e) {
-      return new Status("ERROR-" + e.getReturnValue(), e.getMessage());
+    } catch (DhtClientBindingException e) {
+      return new Status("ERROR ", e.getMessage());
     }
     return Status.OK;
   }
 
   @Override
   public Status delete(String table, String key) {
-
+    
     try {
+      
       // very simple function, just remove the data associated with the key given. table is not used.
       // Example: ioctx.remove(key);
       DhtClientBinding.delete(key);
-    } catch (RadosException e) {
-      return new Status("ERROR-" + e.getReturnValue(), e.getMessage());
+    } catch (DhtClientBindingException e) {
+      return new Status("ERROR ", e.getMessage());
     }
     return Status.OK;
   }
 
   @Override
   public Status update(String table, String key, Map<String, ByteIterator> values) {
+    
     // Just call delete() function first and then call insert() function.
     // No need to change the code.
     Status rtn = delete(table, key);
@@ -116,8 +126,7 @@ public class DecentDHTClient extends DB {
   }
 
   @Override
-  public Status scan(String table, String startkey, int recordcount, Set<String> fields,
-      // I guess we don't need to implement this as well. lol.
+  public Status scan(String table, String startkey, int recordcount, Set<String> fields, 
       Vector<HashMap<String, ByteIterator>> result) {
     return Status.NOT_IMPLEMENTED;
   }
